00f0a1cd11de034ee5170e757ef207c89669c3ce
AlessandroPuccini <puccini.alessandro@gmail.com>
2025-05-22 15:41:25 +0200

    creazione footer

--- elena/src/components/Footer.jsx
+++ elena/src/components/Footer.jsx
@@ -2,10 +2,14 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
+    <div id='footer'>
+      <div className='contacts'>
       <h4>elenamedori.design@gmail.com</h4>
       <h4>+393494708859</h4>
+      </div>
+      <div className='copyright'>
       <h6>© 2025 Elena Medori</h6>
+      </div>
     </div>
   )
 }
