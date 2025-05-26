e560c9c20d47b3e464b9a2c1c7df48fd609a8078
AlessandroPuccini <puccini.alessandro@gmail.com>
2025-05-26 11:43:53 +0200

    mdifiche css in navbar, root font

--- elena/src/components/NavBar.jsx
+++ elena/src/components/NavBar.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom'
 
 function NavBar() {
+
+  // const [isTrasparent, setIsTrasparent ] = useState (false)
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     setIsTrasparent(window.scrollY > 50)
+  //   }
+  //   window.addEventListener('scroll', handleScroll)
+  //   return ()=> window.removeEventListener('scroll', handleScroll)
+  // }, [])
+
   return (
-    <nav>
+    <nav className='sticky-navbar'>
       <div className='logo-name'>
         <Link to="/">
-          <h2>Elena Medori</h2>
+          <h2>ELENA MEDORI</h2>
         </Link></div>
-        <div className='menu-items'> 
+        <div className='menu-items'>  
       <ul>
         <li><Link to='/about'>ABOUT</Link></li>
+        <li><Link to='/commercials'>COMMERCIALS</Link></li>
+        <li><Link to='/movies'>MOVIES</Link></li>
+        <li><Link to='/stills'>STILLS</Link></li>
       </ul>
       </div>
     </nav>
