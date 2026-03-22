9e72d1e5ca01705018a0805adb2aa7a5873e70fb
Alessandro Puccini <alessandropuccini@Mac.station>
2026-03-22 23:17:06 +0100

    correzzioni foto home da server

--- elena/src/components/Home.jsx
+++ elena/src/components/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
         <img className="gallery-img" src="https://ik.imagekit.io/ae2125/elenamedori/despar2.png" alt="despar"/>
         <img className="gallery-img" src="https://ik.imagekit.io/ae2125/elenamedori/InteriorD1.jpg" alt="Interior" />
         <img className="gallery-img" src="https://ik.imagekit.io/ae2125/elenamedori/commercials/amadori2.png" alt="Amadori" />
-        <img className="gallery-img" src="https://ik.imagekit.io/ae2125/elenamedori/InteriorD4.jpg" alt="Interior" />
+        <img className="gallery-img" src="https://ik.imagekit.io/ae2125/elenamedori/interiorD4.png" alt="Interior" />
         <img className="gallery-img" src="https://ik.imagekit.io/ae2125/elenamedori/film/DonnaSola3.png" alt="L'ospite" />
         <img className="gallery-img" src="https://ik.imagekit.io/ae2125/elenamedori/mym2.jpg" alt="stillife" />
         <img className="gallery-img" src="https://ik.imagekit.io/ae2125/elenamedori/mym.jpg" alt="adv mym" />
